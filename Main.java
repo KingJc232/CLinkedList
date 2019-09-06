5cbb798d7f0c6e020d05866ac9d9d4f8c4883c4e
Jc <jose@DESKTOP-P2GJLVR.localdomain>
2019-09-06 12:49:13 -0700

    First Commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/** Implementing a Circularly Linked List 
+
+	Using a Sigularly Linked List Under the Hood 
+
+	And 
+
+	Using a Doubly Linked List Under the Hood
+*/
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
